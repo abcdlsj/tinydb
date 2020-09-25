2bfd67c7923a00d3484060ab21d94de700d62f37
abcdlsj <lisongjianshuai@gmail.com>
2020-09-25 13:08:42 +0800

    add statement && prepare_parsed

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+  StatementType type;
+} Statement;
 
 typedef struct {
   char *buf;
@@ -10,8 +22,8 @@ typedef struct {
   size_t in_len;
 } Ibuffer;
 
-Ibuffer* new_in_buffer() {
-  Ibuffer* in_buffer = malloc(sizeof(Ibuffer));
+Ibuffer *new_in_buffer() {
+  Ibuffer *in_buffer = malloc(sizeof(Ibuffer));
   in_buffer->buf_len = 0;
   in_buffer->buf = NULL;
   in_buffer->in_len = 0;
@@ -19,11 +31,9 @@ Ibuffer* new_in_buffer() {
   return in_buffer;
 }
 
-void print_prompt() {
-  printf("[tinydb]\n>> ");
-}
+void print_prompt() { printf("[tinydb]\n>> "); }
 
-void read_input(Ibuffer* buffer) {
+void read_input(Ibuffer *buffer) {
   ssize_t bytes = getline(&(buffer->buf), &(buffer->buf_len), stdin);
 
   if (bytes < 0) {
@@ -31,29 +41,74 @@ void read_input(Ibuffer* buffer) {
     exit(EXIT_FAILURE);
   }
 
-
   buffer->in_len = bytes - 1;
   buffer->buf[bytes - 1] = 0;
 }
 
-
-void close_buffer(Ibuffer* buffer) {
+void close_buffer(Ibuffer *buffer) {
   free(buffer->buf);
   free(buffer);
 }
 
-int main () {
-  Ibuffer* buffer = new_in_buffer();
+MetaCommandResult do_meta_command(Ibuffer *buffer) {
+  if (strcmp(buffer->buf, ".exit") == 0) {
+    close_buffer(buffer);
+    exit(EXIT_FAILURE);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
 
+PrepareResult prepare_statement(Ibuffer *buffer, Statement statement) {
+  if (strncmp(buffer->buf, "insert", 6) == 0) {
+    statement.type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strncmp(buffer->buf, "select", 6) == 0) {
+    statement.type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+  switch (statement->type) {
+  case STATEMENT_INSERT:
+    printf("insert.\n");
+    break;
+  case STATEMENT_SELECT:
+    printf("select.\n");
+    break;
+  }
+}
+
+int main() {
+  Ibuffer *buffer = new_in_buffer();
   for (;;) {
     print_prompt();
     read_input(buffer);
 
-    if (strcmp(buffer->buf, ".exit") == 0) {
-      close_buffer(buffer);
-      exit(EXIT_FAILURE);
-    } else {
-      printf("Unrecognized command '%s'.\n", buffer->buf);
+    if (buffer->buf[0] == '.') {
+      switch (do_meta_command(buffer)) {
+      case META_COMMAND_SUCCESS:
+        continue;
+      case META_COMMAND_UNRECOGNIZED_COMMAND:
+        printf("Unrecognized command '%s'\n", buffer->buf);
+        continue;
+      }
     }
+
+    Statement statement;
+    switch (prepare_statement(buffer, statement)) {
+    case PREPARE_SUCCESS:
+      break;
+    case PREPARE_UNRECOGNIZED_STATEMENT:
+      printf("Unrecognized keyword at start of '%s'.\n", buffer->buf);
+      continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
